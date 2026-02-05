8d78dda402890c5656034e5c7eb1e074531cc3e7
take_ryosuke <take.ryosuke@alhinc.jp>
2026-02-05 17:22:25 +0900

    first commit

---
+++ src/main/java/com/example/teamyamatake/TeamYamatakeApplication.java
@@ -0,0 +1,13 @@
+package com.example.teamyamatake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamYamatakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamYamatakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teamyamatake/TeamYamatakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamyamatake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamYamatakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
