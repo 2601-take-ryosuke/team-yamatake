f5df6877013d0871cbc0a671ad0bdfd186aa941f
yamamoto_takuro <yamamoto.takuro@alhinc.jp>
2026-02-12 13:12:35 +0900

    fix: global exception handler

--- src/main/java/com/teamyamatake/advice/GlobalExceptionHandler.java
+++ src/main/java/com/teamyamatake/advice/GlobalExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.teamyamatake.advice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -7,8 +10,13 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public String handleTypeMismatch(RedirectAttributes redirectAttributes) {
+
+    // 型変換,パラメータ不足の処理用
+    @ExceptionHandler({
+            MethodArgumentTypeMismatchException.class,
+            MissingServletRequestParameterException.class,
+    })
+    public String handleBadRequest(Exception ex, RedirectAttributes redirectAttributes) {
         redirectAttributes.addFlashAttribute("errorMessage", "不正なパラメータです");
         return "redirect:/";
     }
