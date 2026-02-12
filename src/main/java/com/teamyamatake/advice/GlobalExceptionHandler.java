7b872c42a9eae6422c12046d526d6ad4c81cdee8
yamamoto_takuro <yamamoto.takuro@alhinc.jp>
2026-02-12 09:41:37 +0900

    feat: add error message

--- src/main/java/com/teamyamatake/advice/GlobalExceptionHandler.java
+++ src/main/java/com/teamyamatake/advice/GlobalExceptionHandler.java
@@ -3,12 +3,13 @@ package com.teamyamatake.advice;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    public String handleTypeMismatch() {
+    public String handleTypeMismatch(RedirectAttributes redirectAttributes) {
+        redirectAttributes.addFlashAttribute("errorMessage", "不正なパラメータです");
         return "redirect:/";
     }
 }
